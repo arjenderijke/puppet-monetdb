c6682205a98e513c133bf2093966be37e60d486a
Arjen de Rijke <arjenderijke@gmail.com>
2015-01-18 12:42:06 +0100

    add code from: puppet module generate

---
+++ manifests/init.pp
@@ -0,0 +1,41 @@
+# == Class: monetdb
+#
+# Full description of class monetdb here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if it
+#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should not be used in preference to class parameters  as of
+#   Puppet 2.6.)
+#
+# === Examples
+#
+#  class { monetdb:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  }
+#
+# === Authors
+#
+# Arjen de Rijke <arjenderijke@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2015 Arjen de Rijke
+#
+class monetdb {
+
+
+}
