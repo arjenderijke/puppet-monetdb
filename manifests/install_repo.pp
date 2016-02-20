d10bc37c468714e4a173c053d823f011a7e073b1
Arjen de Rijke <arjenderijke@gmail.com>
2016-02-20 14:49:47 +0100

    repository settings depend on os

--- manifests/install_repo.pp
+++ manifests/install_repo.pp
@@ -6,10 +6,17 @@
 
 class monetdb::install_repo inherits monetdb::params {
 
-  file {'/etc/yum.repos.d/monetdb.repo':
-    source => 'puppet:///modules/${module_name}/monetdb.repo',
-    owner  => root,
-    group  => root,
-    mode   => '0644',
+  case $::osfamily {
+    'RedHat' : {
+      file {'/etc/yum.repos.d/monetdb.repo':
+        source => 'puppet:///modules/${module_name}/monetdb.repo',
+        owner  => root,
+        group  => root,
+        mode   => '0644',
+      }
+    }
+    default : {
+      fail('Operating System not supported')
+    }
   }
 }
--- spec/classes/monetdb_spec.rb
+++ spec/classes/monetdb_spec.rb
@@ -2,19 +2,28 @@ require 'spec_helper'
 
 describe 'monetdb' do
 
-  it do
-    should contain_service('monetdbd').with(
-                                                  'ensure' => 'running',
-                                                  'enable' => true
-                                                  )
-  end
+  context 'on fedora 22' do
+    let(:facts) do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystem        => 'Fedora',
+        :operatingsystemrelease => '22'
+      }
+    end
 
-  it do
-    should contain_file('/etc/yum.repos.d/monetdb.repo').with(
-                                                  'owner' => 'root',
-                                                  'group' => 'root',
-                                                  'mode' => '0644'
-                                                  )
-  end
+    it do
+      should contain_service('monetdbd').with(
+               'ensure' => 'running',
+               'enable' => true
+             )
+    end
 
+    it do
+      should contain_file('/etc/yum.repos.d/monetdb.repo').with(
+               'owner' => 'root',
+               'group' => 'root',
+               'mode' => '0644'
+             )
+    end
+  end
 end
