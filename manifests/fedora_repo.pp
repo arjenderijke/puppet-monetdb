d3195eea9de7328012bd9a6db7b8807ed1e583b9
Arjen de Rijke <arjenderijke@gmail.com>
2016-02-22 19:57:03 +0100

    start defining repository file using standard resources

---
+++ manifests/fedora_repo.pp
@@ -0,0 +1,19 @@
+#
+# === Copyright
+#
+# Copyright 2015 Arjen de Rijke
+#
+
+class monetdb::monetdb_repo inherits monetdb::params {
+
+  yumrepo {'MonetDB $releasever - $basearch':
+    ensure   => present,
+    name     => 'MonetDB $releasever - $basearch',
+    baseurl  => 'http://dev.monetdb.org/downloads/Fedora/$releasever/$basearch/',
+    enabled  => true,
+    gpgcheck => true,
+    gpgkey   => 'https://www.monetdb.org/downloads/MonetDB-GPG-KEY',
+    target   => '/etc/yum.repos.d/monetdb.repo',
+  }
+
+}
