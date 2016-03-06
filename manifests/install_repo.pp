0135c8503087fb0daa018692c85fedea7264f8b6
Arjen de Rijke <arjenderijke@gmail.com>
2016-03-06 11:01:31 +0100

    use defined function to setup repo file

--- manifests/install_repo.pp
+++ manifests/install_repo.pp
@@ -8,11 +8,17 @@ class monetdb::install_repo inherits monetdb::params {
 
   case $::osfamily {
     'RedHat' : {
+      monetdb::fedora_repo {'monetdb_fedora_repo':
+        enable_debug   => $::monetdb::params::enable_debug,
+        enable_source  => $::monetdb::params::enable_source,
+        enable_testing => $::monetdb::params::enable_testing,
+      }
+
       file {'/etc/yum.repos.d/monetdb.repo':
-        source => 'puppet:///modules/${module_name}/monetdb.repo',
-        owner  => root,
-        group  => root,
-        mode   => '0644',
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        require => Monetdb::Fedora_repo['monetdb_fedore_repo'],
       }
     }
     default : {
